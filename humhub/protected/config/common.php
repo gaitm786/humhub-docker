644b9f9909109cdea82b33caa1cd27fd20370533
Andrew Johnson <blackandred@users.noreply.github.com>
2020-03-31 18:50:40 +0200

    Fix autoloading - @force-rebuild

--- humhub/protected/config/common.php
+++ humhub/protected/config/common.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/var/www/localhost/humhub-autoload/vendor/autoload.php';
+require_once '/var/www/localhost/humhub-composer/vendor/autoload.php';
 
 /**
  * This file provides to overwrite the default HumHub / Yii configuration by your local common (Console and Web) environments
